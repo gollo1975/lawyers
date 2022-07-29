1ef4e5f3f4b2d0e180d4b1fdca0402a8148e3c47
gollo1975 <jgpmorales1975@hotmail.com>
2022-07-29 14:06:53 -0500

    Add files via upload

---
+++ assets/AppAsset.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main application asset bundle.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        //'css/site.css',
+        //'css/skins/_all-skins.min.css',
+        //'plugins/select-2/css/select2.min.css',
+        //'plugins/font-awesome/css/font-awesome.min.css',
+        //'login/style.ccs'
+    ];
+    public $js = [
+        //'js/main.js',
+        //'js/commons.js',
+        'plugins/select-2/js/select2.min.js',
+        'plugins/tinymce/tinymce.min.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset'
+    ];
+}
