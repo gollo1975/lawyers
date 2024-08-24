26fefaf62a276cf6641d62443cc131374021c33e
gollo1975 <jgpmorales1975@hotmail.com>
2024-08-24 17:11:12 -0500

    bd

--- config/db.php
+++ config/db.php
@@ -6,6 +6,7 @@ return [
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
+    'attributes' => [PDO::ATTR_CASE => PDO::CASE_LOWER],
     
   /*  'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=181.129.107.162;dbname=maquiladigital',
